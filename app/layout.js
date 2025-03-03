90bbbd6b272a4290701afbc258958cd06fd5768a
Utkarsh Rana <utkarshranaa06@gmail.com>
2025-03-03 05:46:05 -0500

    updated all the project files for deployment

---
+++ app/layout.js
@@ -0,0 +1,34 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Provider from "./provider";
+import { Toaster } from "@/components/ui/sonner";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "MailGenie- Professional AI Email Builder",
+  description: "Mail Genie: Effortlessly transform simple prompts into professional, ready-to-send emails.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Provider>
+          {children}
+        </Provider>
+        <Toaster />
+      </body>
+    </html>
+  );
+}
